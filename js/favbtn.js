cd4ee56255c210ac279ceb6fc627f58c7b160edb
s182959 <54835142+s182959@users.noreply.github.com>
2019-10-28 13:11:58 +0100

    Add files via upload

---
+++ js/favbtn.js
@@ -0,0 +1,27 @@
+function rudr_favorite(a) {
+	pageTitle=document.title;
+	pageURL=document.location;
+	try {
+		// Internet Explorer solution
+		eval("window.external.AddFa-vorite(pageURL, pageTitle)".replace(/-/g,''));
+	}
+	catch (e) {
+		try {
+			// Mozilla Firefox solution
+			window.sidebar.addPanel(pageTitle, pageURL, "");
+		}
+		catch (e) {
+			// Opera solution
+			if (typeof(opera)=="object") {
+				a.rel="sidebar";
+				a.title=pageTitle;
+				a.url=pageURL;
+				return true;
+			} else {
+				// The rest browsers (i.e Chrome, Safari)
+				alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
+			}
+		}
+	}
+	return false;
+}
